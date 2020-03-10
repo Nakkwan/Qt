35e82e2f34d8544ea2fe1070661ea129b275afbd
Nakkwan <cvvc1997@gmail.com>
2020-03-11 01:18:22 +0900

    Qt tutorial 6

---
+++ Qtproject/QMessageBoxExample/mainwindow.cpp
@@ -0,0 +1,41 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    //void              about
+    //void              aboutQt
+    //StandardButton    critical
+    //StandardButton    information
+    //StandardButton    question
+    //StandardButton    warning
+
+    //QMessageBox::about(this, "My title", "This is my custom message");
+    //QMessageBox::aboutQt(this, "My title");
+    //QMessageBox::critical(this,"My title", "This is my custom message");
+    //QMessageBox::information(this,"My title", "This is my custom message");
+    //QMessageBox::question(this,"My title", "This is my custom message", QMessageBox::Yes | QMessageBox::No);
+    //QMessageBox::warning(this,"My title", "This is my custom message");
+    QMessageBox::StandardButton reply = QMessageBox::question(this,"My title", "This is my custom message",
+                                                              QMessageBox::Yes | QMessageBox::No);
+    if(reply == QMessageBox::Yes){
+        QApplication::quit();
+    } else{
+        qDebug() << "Nothing is clicked";
+    }
+
+}
