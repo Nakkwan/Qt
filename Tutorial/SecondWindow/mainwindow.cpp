1758d0a9ef79ad3a2cb5d1231f5adc4cf74dd130
Nakkwan <cvvc1997@gmail.com>
2020-03-13 15:29:16 +0900

    tutorial splitter, secWindow

---
+++ Tutorial/SecondWindow/mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+//#include "secwin.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+//    //if secWindow opened, I can't approach mainWindow until secWindow closed1
+//    //Because it will created by stack object
+//    SecWin secWindow;
+//    secWindow.setModal(true);
+//    secWindow.exec();
+
+    hide();         //hide mainwindow when pushbutton clicked
+
+    //create pointer in mainwindow.h for create object by heap
+    secWindow = new SecWin(this);
+    secWindow->show();
+
+}
